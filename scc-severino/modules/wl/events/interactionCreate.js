53d055b295c93571f46c186c36d1d5a7725e1ebb
JulianoPassing <julianosalm@gmail.com>
2025-07-22 14:17:05 -0300

    att
    att

--- scc-severino/modules/wl/events/interactionCreate.js
+++ scc-severino/modules/wl/events/interactionCreate.js
@@ -146,7 +146,7 @@ export default async function(client) {
             titulo: '8. O que é Safe Zone?',
             desc: 'Área Segura',
             alternativas: {
-              a: 'Locais onde crimes são proibidos',
+              a: 'Locais onde crimes são liberados',
               b: 'Locais seguros',
               c: 'Área só para administradores'
             }

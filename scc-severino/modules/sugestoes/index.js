efeeb86c4035cc3eecd46f4ff812e61f2aad4943
JulianoPassing <julianosalm@gmail.com>
2025-07-24 23:43:31 -0300

    Update index.js

--- scc-severino/modules/sugestoes/index.js
+++ scc-severino/modules/sugestoes/index.js
@@ -20,9 +20,7 @@ const setupSugestoesModule = function(client) {
           iconURL: message.author.displayAvatarURL({ dynamic: true, size: 64 })
         })
         .setTitle('💡 Sugestão')
-        .setDescription(`
-´´´${conteudo}´´´
-`)
+        .setDescription(```\n${conteudo}\n```)
         .addFields(
           { name: '👤 Autor', value: `<@${message.author.id}>`, inline: true },
           { name: '📅 Data', value: `<t:${Math.floor(Date.now() / 1000)}:F>`, inline: true }
